9fa1bbeb9ec0864e27b1842b5bd71e05a937ecec
PaulCardoos <paulcardoos@gmail.com>
2022-02-09 16:42:15 -0500

    added file logic, for loop, match

--- src/item.rs
+++ src/item.rs
@@ -1,34 +1,32 @@
-#[derive(Debug)]
+#[derive(Default, Debug, Clone)]
 pub struct Item {
     name : String,
     price : f32,
-    description : String, 
+    description : Option<String>, 
 }
 
 impl Item {
-    pub fn new() -> Self {
-        let i =  Item{
-            name : String::from(""),
-            price : 0.00, 
-            description : String::from(""),
+    pub fn new(item_name:String, item_price:f32) -> Item {
+        let i = Item{
+            name : item_name,
+            price : item_price, 
+            description : None,
         };
         i
-    }
-
-    pub fn item_name(mut self, name:String) -> Self {
-        self.name = name;
-        self
         
     }
 
-    pub fn item_price(mut self, price:f32) -> Self {
-        self.price = price;
+    pub fn item_description(mut self, description:String) -> Self {
+        self.description = Some(description);
         self
     }
 
-    pub fn item_description(mut self, description:String) -> Self {
-        self.description = description;
-        self
+    pub fn build(self) -> Self{
+        Item {
+            name : self.name, 
+            price : self.price,
+            description : self.description,
+        }
     }
 
     pub fn print_item(&self){
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
 mod item;
+use std::io::stdin;
+use std::fs::OpenOptions;
+use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
-    let baba = item::Item::new().item_name(String::from("Hummes"))
-    .item_price(5.00)
-    .item_description(String::from("Chickpeas are the shizzz"));
+
+    // check for existing items from the file
+    let file = match OpenOptions::new()
+    .read(true)
+    .write(true)
+    .create(true)
+    .open("items.txt"){
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file, {:?}", error),
+    };
+
+    // read file contents into a vector
+    
+ 
+    
+    loop {
+        let mut user_choice = String::new();
+        stdin().read_line(&mut user_choice).expect("Could not read line");
+        let num : u8 = match user_choice.trim().parse(){
+            Ok(num) => num,
+            Err(err) => panic!("Did understand, {:?}", err),
+        };
+
+        match &num {
+            0 => println!("Enter a valid choice"),
+            1 => println!("God"), 
+            2 => println!("Loves"),
+            3 => println!("Me"),
+            4 => break,
+            5_u8..=u8::MAX => println!("Enter a valid choice")
+        }
+    }
 
     
-    baba.print_item();
+
     
 
 }
